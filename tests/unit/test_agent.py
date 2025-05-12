a63a2426488336da9d9800ec110c37cb92764fb8
Brijesh Prajapati <brijesh@incubyte.co>
2025-05-12 12:31:15 +0530

    feat/convert test in class based

--- tests/unit/test_agent.py
+++ tests/unit/test_agent.py
@@ -1,28 +1,32 @@
-from service.agent_service import AgentService
+import pytest
 from fastapi.testclient import TestClient
+from service.agent_service import AgentService
 from main import app
 
 
-# Mock the AgentService for testing
 class MockAgentService:
     def generate_response(self, prompt: str) -> str:
         return f"Mocked response to: {prompt}"
-    
-app.dependency_overrides[AgentService] = lambda: MockAgentService()
 
-client = TestClient(app)
 
-def test_run_agent():
-    payload = {"prompt": "Hello agent!"}
-    response = client.post("/agent", json=payload)
-    assert response.status_code == 200
-    assert response.json() == {"response": "Mocked response to: Hello agent!"}
+@pytest.fixture(scope="class")
+def test_client():
+    app.dependency_overrides[AgentService] = lambda: MockAgentService()
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides = {}
 
-def test_run_agent_empty_prompt_throw_error():
-    payload = {"prompt": ""}
-    response = client.post("/agent", json=payload)
-    assert response.status_code == 401
-    assert response.json() == {
-        "detail": "prompt must not be empty"
-    }
 
+@pytest.mark.usefixtures("test_client")
+class TestAgent:
+    def test_run_agent(self, test_client):
+        payload = {"prompt": "Hello agent!"}
+        response = test_client.post("/agent", json=payload)
+        assert response.status_code == 200
+        assert response.json() == {"response": "Mocked response to: Hello agent!"}
+
+    def test_run_agent_empty_prompt_throw_error(self, test_client):
+        payload = {"prompt": ""}
+        response = test_client.post("/agent", json=payload)
+        assert response.status_code == 401
+        assert response.json() == {"detail": "prompt must not be empty"}
