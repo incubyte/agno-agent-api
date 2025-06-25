6baee1b54fd125f3d0ae94cd48274b0e44e564ed
Brijesh Prajapati <brijesh@incubyte.co>
2025-06-25 13:55:24 +0530

    fix: remove unnecessary option which fails production

--- app/db/engine.py
+++ app/db/engine.py
@@ -2,8 +2,11 @@ from sqlmodel import Session, create_engine
 from app.core.setting import settings
 
 
+print("Initializing database engine...")
+print(f"Using database URL: {settings.DATABASE_URL}")
+print("sender Email:", settings.SENDER_EMAIL)
 engine = create_engine(
-    settings.DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.DATABASE_URL, connect_args={}
 )
 
 session  = Session(bind=engine)
