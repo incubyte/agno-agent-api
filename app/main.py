71db856c2b6fde5d07efd5154685545d3cf207d3
Brijesh Prajapati <brijesh@incubyte.co>
2025-06-25 12:51:33 +0530

    feat: allow specific origin

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ app.include_router(agent_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://illustrious-pasca-7cb1b5.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
